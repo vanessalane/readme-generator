4c8827e87e8bb57d704326fc6857d2482d537495
Vanessa Lane <vlane@23andme.com>
2020-07-19 18:45:02 -0700

    write response to markdown file

--- index.js
+++ index.js
@@ -1,15 +1,65 @@
-// array of questions for user
-const questions = [
 
-];
+const inquirer = require("inquirer");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, generateMarkdown(data), err => {
+            if (err) {
+                reject(err);
+                return;
+            };
+
+            resolve({
+                ok: true,
+                message: `Successfully created your README, ${fileName}!`
+            });
+        });
+    });
 }
 
 // function to initialize program
 function init() {
+    // get the commonly used licenses from GitHub
+    fetch("https://api.github.com/licenses")
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(licenseData) {
+                // create a question that prompts for the licenses
+                const licensePrompt = {
+                    type: 'list',
+                    name: 'field',
+                    message: `license:`,
+                    choices: licenseData.map( license => license.name )
+                }
 
+                // add it to the questions array
+                questions.push(licensePrompt);
+
+                // start the prompt
+                console.log(`
+                    ==================================
+                     Provide details for a new README
+                    ==================================
+                `);
+                return inquirer.prompt(questions)
+                    .then(responses => {
+                        const projectName = `${responses.title}_README.md`;
+                        return writeToFile(projectName, responses).then(writeFileResponse => {
+                                console.log(writeFileResponse.message);
+                            })
+                            .catch(err => {
+                                console.log(err);
+                            });
+                    });
+            })
+        } else {
+            console.log(`Licenses could not be loaded: ${err}`);
+        }
+    });
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,34 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${data.installation_instructions}
+  
+  ## Usage
+  ${data.usage_information}
+  
+  ## License
+  This application is covered under ${data.license}.
+  
+  ## Contributing
+  ${data.contribution_guidelines}
+  
+  ## Tests
+  ${data.testing_information}
+  
+  ## Questions
+  Questions? Email ${data.email}.
 `;
 }
 
